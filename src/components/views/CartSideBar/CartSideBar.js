93984b955de587f0d576b2e454e29a01e555daf2
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-06-16 22:16:38 +0200

    display number of products in the cartSideBar

--- src/components/views/CartSideBar/CartSideBar.js
+++ src/components/views/CartSideBar/CartSideBar.js
@@ -3,49 +3,52 @@ import PropTypes from 'prop-types';
 
 import clsx from 'clsx';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import { connect } from 'react-redux';
+import { getCart } from '../../../redux/cartRedux.js';
 
 import styles from './CartSideBar.module.scss';
 // import { Button } from '../Button/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
-const Component = ({className, children}) => (
-  <section className={clsx(className, styles.root)}>
-    <div className={styles.iconbascet} >
-      <FontAwesomeIcon
-        icon={faCartPlus}
-        className={styles.bascet}
-      />
-      8
-    </div>
-    <div className={styles.cartOpen}>
-      <div className={`${styles.cart} cart sm-12 `}>
-        <div className={styles.cartItems}>
+const Component = ({ className, cart }) => {
+  console.log('cart', cart);
+
+  return (
+    <section className={clsx(className, styles.root)}>
+      <div className={styles.iconbascet} >
+        <FontAwesomeIcon
+          icon={faCartPlus}
+          className={styles.bascet}
+        />
+        <span>{cart.products.length}</span>
+      </div>
+      <div className={styles.cartOpen}>
+        <div className={`${styles.cart} cart sm-12 `}>
+          <div className={styles.cartItems}>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 Component.propTypes = {
-  children: PropTypes.node,
+  cart: PropTypes.object,
   className: PropTypes.string,
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  cart: getCart(state),
+});
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
 // });
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps)(Component);
 
 export {
-  Component as CartSideBar,
-  // Container as CartSideBar,
+  Container as CartSideBar,
   Component as CartSideBarComponent,
 };
--- src/components/views/CartSideBar/CartSideBar.test.js
+++ src/components/views/CartSideBar/CartSideBar.test.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { CartSideBarComponent } from './CartSideBar';
 
+const mockProps = {
+  cart: {
+    products: [],
+    amount: 5,
+  },
+};
+
 describe('Component CartSideBar', () => {
   it('should render without crashing', () => {
-    const component = shallow(<CartSideBarComponent />);
+    const component = shallow(<CartSideBarComponent {...mockProps}/>);
     expect(component).toBeTruthy();
   });
 });
