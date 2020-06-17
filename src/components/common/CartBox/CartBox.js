11ec15d0c59850a6b54a4317cee7adceec107b4e
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-06-17 23:21:30 +0200

    remmove from cart

--- src/components/common/CartBox/CartBox.js
+++ src/components/common/CartBox/CartBox.js
@@ -3,15 +3,14 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
 import { connect } from 'react-redux';
-import { getCart, changeAmount } from '../../../redux/cartRedux.js';
-import { addNotes } from '../../../redux/cartRedux.js';
+import { getCart, changeAmount, addNotes, removeProduct } from '../../../redux/cartRedux.js';
 
 import styles from './CartBox.module.scss';
 import { AmountWidget } from '../../common/AmountWidget/AmountWidget';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  faTrash } from '@fortawesome/free-solid-svg-icons';
 
-const Component = ({ className, cart, changeAmount, addNotes }) => {
+const Component = ({ className, cart, changeAmount, addNotes, removeProduct }) => {
 
   return (
     <section className={clsx(className, styles.root)}>
@@ -27,9 +26,12 @@ const Component = ({ className, cart, changeAmount, addNotes }) => {
             <div className={` ${styles.cartDesc} d-flex justify-content-between `} >
               <span span id='cart-item-price '>$ { el.price * el.amount} </span>
               <AmountWidget className={styles.amountWidget} value={el.amount} onChange={e => changeAmount({ id: el.id, amount: parseInt(e.target.value) })} />
-              <a href='/' id='cart-item-remove ' className={ styles.cartItemRemove }>
-                <FontAwesomeIcon icon={faTrash} />
-              </a>
+              <div id='cart-item-remove ' className={ styles.cartItemRemove }>
+                <FontAwesomeIcon
+                  icon={faTrash}
+                  onClick={() => removeProduct({ id: el.id })}
+                />
+              </div>
             </div>
           </div>
           <div className=' d-flex justify-content-center pb-3 '>
@@ -55,6 +57,7 @@ Component.propTypes = {
   cart: PropTypes.object,
   changeAmount: PropTypes.func,
   addNotes: PropTypes.func,
+  removeProduct: PropTypes.func,
 };
 
 const mapStateToProps = state => ({
@@ -64,7 +67,7 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   changeAmount: (id, amount) => dispatch(changeAmount(id, amount)),
   addNotes: (id, notes) => dispatch(addNotes(id, notes)),
-
+  removeProduct: (id) => dispatch(removeProduct(id)),
 });
 
 const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
